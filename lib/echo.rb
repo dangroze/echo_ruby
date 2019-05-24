5c41bf52b0d5add22ecad44d30a7551ff8992956
dangroze <dangroze@gmail.com>
2019-05-24 15:30:50 +0100

    :sparkles:Passed tests

--- lib/echo.rb
+++ lib/echo.rb
@@ -5,12 +5,26 @@ class Echo
   end
 
   def answer
-    @response = STDIN.gets.chomp
+    @a = STDIN.gets.chomp
   end
 
   def engine
-    prompt
-    self.answer
-    Time.now.strftime("%Y-%m-%d | %H:%M | You said: #{self.answer}")
+    loop do
+      prompt
+      answer
+      if @a == "exit"
+        puts "Goodbye"
+        break
+      else
+        time_converter
+      end
+    end    
   end
+  
+  private
+
+  def time_converter
+    puts Time.now.strftime("%Y-%m-%d | %H:%M | You said: #{@a}")
+  end
+
 end
