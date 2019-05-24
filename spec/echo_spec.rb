55474a4af376a29ddeb5be58fef9e1af7ab864fb
dangroze <dangroze@gmail.com>
2019-05-24 13:18:50 +0100

    :white_check_mark:Add second test

--- spec/echo_spec.rb
+++ spec/echo_spec.rb
@@ -1,11 +1,16 @@
 require 'echo'
 
 RSpec.describe Echo do
-  let(:subject) {described_class.new}
+  let(:echo) {described_class.new}
   context '#prompt' do
-    it 'ask you to say something' do
-      expect{subject.prompt}.to output('Say something!').to_stdout
+    it 'asks you to say something' do
+      expect{echo.prompt}.to output('Say something: ').to_stdout
     end
   end
-
+  context '#engine' do
+    it "user can input an answer" do
+      allow(STDIN).to receive(:gets).and_return('hello')
+      expect(echo.engine).to eq('hello')  
+    end
+  end
 end
